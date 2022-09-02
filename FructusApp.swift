1d7178b75bfb30191c6c7bf4ceae3ee161f40dbb
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-02 18:35:06 +0900

    Initial commit

---
+++ FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 大久保樹 on 2022/09/02.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
