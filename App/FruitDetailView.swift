f262525a2366dc1d4e24366dc72310b0bbffd57b
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-04 17:05:31 +0900

    Swift進捗
    Fructusアプリの作成

---
+++ App/FruitDetailView.swift
@@ -0,0 +1,52 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by 大久保樹 on 2022/09/04.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    // MARK: - PROPERTIES
+    
+    var fruit: Fruit
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .center, spacing: 20) {
+                    // HEADER
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        // TITLE
+                        Text(fruit.title)
+                        
+                        // HEADLINE
+                        
+                        // NUTRIENTS
+                        
+                        // SUBHEADLINE
+                        
+                        // DESCRIPTION
+                        
+                        // LINK
+                        
+                    } //: VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                } //: VSTACK
+            } //: SCROLL
+        } //: NAVIGATION
+    }
+}
+
+// MARK: - PREVIEW
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView(fruit: fruitsData[0])
+    }
+}
