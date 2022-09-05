6ea5f4c1a4b62dc391e0ef193478480996dd986f
Ituking <77528963+Ituking@users.noreply.github.com>
2022-09-05 20:52:12 +0900

    Swift進捗
    Fructusアプリの作成

---
+++ View/SettingsLabelView.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsLabelView.swift
+//  Fructus
+//
+//  Created by 大久保樹 on 2022/09/05.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    // MARK: - PROPERTIES
+    
+    var labelText: String
+    var labelImage: String
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack {
+            Text(labelText.uppercased()).fontWeight(.bold)
+            Spacer()
+            Image(systemName: labelImage)
+        }
+    }
+}
+
+// MARK: - PREVIEW
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fructus", labelImage: "Info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
